49fb2bc057c329136b2724515d35178aa6738cca
Iriska074 <102680449+Iriska074@users.noreply.github.com>
2024-12-08 04:02:59 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,29 +1,24 @@
 from telegram import Update, KeyboardButton, ReplyKeyboardMarkup
 from telegram.ext import Application, CommandHandler, CallbackContext
 
-# Ваш токен от BotFather
 TOKEN = "7756439379:AAFTcllKuFy2SpRdCoELAhRvP-zt6P1ZARA"
 
 async def start(update: Update, context: CallbackContext):
-    # Кнопка для запуска мини-приложения
     keyboard = [
-        [KeyboardButton("Запустить игру", web_app={"url": "https://iriska074.github.io/Metal-Shift/"})]
+        [KeyboardButton("Запустить игру", web_app={"url": "https://iriska074.github.io/Metal-Shift/game.html"})]
     ]
     reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
 
-    # Отправляем сообщение с кнопкой
-    await update.message.reply_text(
-        "Нажмите на кнопку ниже, чтобы начать игру!", reply_markup=reply_markup
-    )
+    await update.message.reply_text("Нажмите на кнопку ниже, чтобы начать игру!", reply_markup=reply_markup)
 
 def main():
-    # Создаём приложение (новый способ с python-telegram-bot 20.x)
+    # Создание приложения
     application = Application.builder().token(TOKEN).build()
 
-    # Обрабатываем команду /start
+    # Добавление обработчика команд
     application.add_handler(CommandHandler("start", start))
 
-    # Запускаем бота
+    # Запуск polling
     application.run_polling()
 
 if __name__ == "__main__":
